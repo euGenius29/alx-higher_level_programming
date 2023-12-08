6cb62679a32fd6950a994bcefb41f7999747c8f5
euGenius29 <angloweugenious29@gmail.com>
2023-12-07 19:31:48 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -31,6 +31,7 @@ void reverse_listint(listint_t **head)
 int is_palindrome(listint_t **head)
 {
 	listint_t *slow = *head, *fast = *head, *temp = *head, dup = NULL;
+
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	while (1)
@@ -49,7 +50,7 @@ int is_palindrome(listint_t **head)
 		slow = slow->next;
 	}
 	reverse_listint(&dup);
-	while(dup && temp)
+	while (dup && temp)
 	{
 		if (temp->n == dup->n)
 		{
@@ -60,6 +61,6 @@ int is_palindrome(listint_t **head)
 			return (0);
 	}
 	if (!dup)
-		return(1);
+		return (1);
 	return (0);
 }
