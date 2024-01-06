e4dcd5f75ec811a8b775fcca8bd6d433342eefc6
euGenius29 <angloweugenious29@gmail.com>
2024-01-05 17:34:17 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+"""
+A class called LockedClass
+"""
 class LockedClass:
+    """
+    A class that prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called 'first_name'.
+    """
     def __setattr__(self, name, value):
+        """
+        Override the default attribute setting behavior.
+
+        Parameters:
+        - name (str): The name of the attribute being set.
+        - value: The value to be assigned to the attribute.
+
+        Raises:
+        - AttributeError: If an attempt is made to set an attribute other than 'first_name'.
+        """
         if name == 'first_name':
             super().__setattr__(name, value)
         else:
