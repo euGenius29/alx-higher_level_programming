03fe4d5a6229ae4756a2ef45d88bcc2c7b2dbe2e
euGenius29 <angloweugenious29@gmail.com>
2024-01-09 10:54:06 -0800

    Adding docstring for method

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 """ Converts JSON string to python object"""
 def from_json_string(my_str):
+    """
+    Returns a python object from a JSON string.
+
+    Args:
+    my_str: json string to create python object from
+
+    Returns:
+    python object.
+    """
     import json
     return(json.loads(my_str))
