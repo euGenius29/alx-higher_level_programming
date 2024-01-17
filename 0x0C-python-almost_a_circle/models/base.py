b942805c236038d51a0dc91134e7e7a932c34a58
euGenius29 <angloweugenious29@gmail.com>
2024-01-17 09:57:36 -0800

    adding a method create to create an instance with set attricutes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -73,3 +73,25 @@ class Base:
             return[]
         else:
             return(json.loads(json_string))
+
+
+    @classmethod
+    def create(cls, **dictionary: dict):
+        """
+        Returns an instance with all attributes already set
+
+        Args:
+        dictionary (dict): reference to instance
+
+        Returns:
+        instance with attributes set.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = None
+
+        dummy.update(**dictionary)
+        return dummy
