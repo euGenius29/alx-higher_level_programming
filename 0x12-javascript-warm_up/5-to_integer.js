cdc142d774540761cbad6fa62e41583784c05c90
euGenius <angloweugenious29@gmail.com>
2024-03-13 14:00:35 +0000

    semistandard fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-if (isNaN(process.argv[2]))
-{
-	console.log('Not a number');
-}
-else
-{
-	console.log("My number: " + Math.floor(Number(process.argv[2])));
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + Math.floor(Number(process.argv[2])));
 }
