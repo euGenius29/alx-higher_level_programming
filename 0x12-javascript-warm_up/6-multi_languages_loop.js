2c9caf6c39828a44b411e54f3eb83195f39c2b68
euGenius <angloweugenious29@gmail.com>
2024-03-13 16:12:52 +0000

    solution to task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-let multi_lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const multiLang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 let i = 0;
-while (i < multi_lang.length)
-{
-	console.log(multi_lang[i]);
-	i++;
+while (i < multiLang.length) {
+  console.log(multiLang[i]);
+  i++;
 }
